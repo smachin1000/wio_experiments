46558d8a66bd2d3c9058652a05bb6fe34b21c85b
Sean Machin <sean.machin@outlook.com>
2021-02-09 12:48:51 -0700

    Added led blink example

---
+++ led_blink/led_blink.ino
@@ -0,0 +1,18 @@
+/*
+  Sample code to blink on-board LED (blue one near USB-C connector)
+  on and of.
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(250);
+}
